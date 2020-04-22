2de574316160e4b2ae6612e2f16f4c31a777250f
edu06 <edu06@EDUPC06.n00d01.kis.co.jp>
2020-04-22 13:19:41 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		                                                                        // TODO 自動生成されたメソッド・スタブ
+		System.out.println("荒木亮太");
+
+		int tuki = 4;
+		int hi =22;
+
+		System.out.println("今日は" + tuki + "月" + hi + "日です。");
+
+		double p =3.14;
+		System.out.println("円周率は" + p + "です。");
+
+
+	}
+
+}
