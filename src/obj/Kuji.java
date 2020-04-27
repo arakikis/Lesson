7045f6379ccb671fcda7ec146d5276ef2c1c922e
edu06 <edu06@EDUPC06.n00d01.kis.co.jp>
2020-04-27 16:28:27 +0900

    4.27宝くじ

---
+++ src/obj/Kuji.java
@@ -0,0 +1,11 @@
+package obj;
+
+public class Kuji {
+	int bangou;
+	String kekka;
+
+	public void show() {
+		System.out.println(bangou+" "+kekka);
+	}
+
+}
---
+++ src/obj/KujiChecker.java
@@ -0,0 +1,19 @@
+package obj;
+
+public class KujiChecker {
+		int kingaku;
+
+		public void check(Kuji k) {
+			if(k.bangou==123456) {
+				k.kekka= "1等";
+				this.kingaku +=100000;
+			}else if(k.bangou % 100 ==12) {
+				k.kekka= "2等";
+				this.kingaku +=1000;
+			}else {
+				k.kekka="はずれ";
+			}
+		}
+
+
+}
